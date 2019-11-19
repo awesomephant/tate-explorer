5f77e57e9d2694ae2d8b80f3d2e68101d85090aa
Max Kohler <max@koehler-kn.de>
2019-11-19 21:14:21 +0000

    build out

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import game from './game.json'
+import util from './util.js'
 import './Game.css'
+
+function fillStores(){
+    for (let i = 0; i < 14; i++){
+        game.graph.push({
+            id: `store1-shelf${i+1}`,
+            back: {node: 'store1'},
+            west: {node: 'corridor1'}
+        })
+    }
+    console.dir(game.graph)
+}
+
+fillStores()
 
 export default class Game extends React.Component {
     constructor(props) {
-        super()
+        super(props)
         this.state = {
             input: '',
             prevInput: '',
             currentRoom: 'entrance',
             outputs: [],
-            inventory: []
+            inventory: game.initialInventory
         }
+
+        this.outputRef = React.createRef()
+        this.inputRef = React.createRef()
+
         this.handleInput = this.handleInput.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.detectSubmit = this.detectSubmit.bind(this)
@@ -20,9 +38,8 @@ export default class Game extends React.Component {
     }
 
     componentDidMount() {
-        this.setState({
-            outputs: [game.rooms[this.state.currentRoom].description]
-        })
+        this.inputRef.current.focus();
+        this.addOutput('You are the archivist. Find TGA431.')
     }
 
     getNodeByID(id) {
@@ -34,8 +51,17 @@ export default class Game extends React.Component {
 
     getItemByID(id) {
         for (let i = 0; i < game.rooms[this.state.currentRoom].items.length; i++) {
-            if (game.rooms[this.state.currentRoom].items[i].id === id) { return game.rooms[this.state.currentRoom].items[i] }
+            if (game.rooms[this.state.currentRoom].items[i].id === id) {
+                return game.rooms[this.state.currentRoom].items[i]
+            }
         }
+        for (let i = 0; i < this.state.inventory.length; i++) {
+            if (this.state.inventory[i].id === id) {
+                return this.state.inventory[i]
+            }
+        }
+
+        return false;
     }
 
     addToInventory(item) {
@@ -44,7 +70,6 @@ export default class Game extends React.Component {
             return prevState
         })
     }
-
     handleChange(e) {
         this.setState({
             input: e.target.value
@@ -65,22 +90,51 @@ export default class Game extends React.Component {
         this.setState(function (prevState) {
             prevState.outputs.push(msg);
             return prevState;
+        }, () => {
+            let outputEl = this.outputRef.current;
+            outputEl.scrollTop = outputEl.scrollHeight;
         })
+    }
+
+    resolveSynonyms(word) {
+        for (let i = 0; i < game.synonyms.length; i++) {
+            for (let j = 0; j < game.synonyms[i].match.length; j++) {
+                if (game.synonyms[i].match[j] === word) {
+                    return game.synonyms[i].term;
+                }
+            }
+        }
+        return word;
+    }
+
+    getMessage(msg) {
+        if (game.messages[msg]) {
+            return game.messages[msg][util.gri(0, game.messages[msg].length - 1)]
+        }
     }
 
     handleInput() {
         this.setState({ prevInput: this.state.input })
-        let command = this.state.input.replace('the ', '').split(' ')
+        let command = this.state.input.split(' ')
 
-        let verb = command[0];
-        let noun = command[1];
+        let verb = this.resolveSynonyms(command[0]);
+        let noun = this.resolveSynonyms(command[1]);
+        let currentNode = this.getNodeByID(this.state.currentRoom);
+
         this.setState(function (prevState) {
             prevState.outputs.push('> ' + this.state.input);
             return prevState;
         })
         this.setState({ input: '' })
-        let currentNode = this.getNodeByID(this.state.currentRoom);
-        let currentRoom = game.rooms[this.state.currentRoom];
+        if (command.length === 1) {
+            noun = this.resolveSynonyms(command[0])
+            if (noun === 'north' || noun === 'south' || noun === 'east' || noun === 'west') {
+                verb = 'go'
+            } else if (noun === 'inventory') {
+                verb = 'examine'
+            }
+        }
+
         if (verb === 'go') {
             // noun is a direction here
             if (currentNode[noun]) {
@@ -95,11 +149,41 @@ export default class Game extends React.Component {
             }
         } else if (verb === 'take') {
             let item = this.getItemByID(noun);
-            if (item) {
+            if (item && item.canTake) {
                 this.addOutput('Taken.')
                 this.addToInventory(item)
+            } if (item && !item.canTake){
+                this.addOutput(this.getMessage('cantTakeThat'))
             } else {
-                this.addOutput("You can't take that.")
+                this.addOutput(this.getMessage('noSuchThing'))
+            }
+        } else if (verb === 'examine') {
+            if (noun === 'inventory') {
+                let list = this.state.inventory.map(function (item) {
+                    return `— ${item.name}`
+                })
+                if (list.length > 0) {
+                    this.addOutput(`You are carrying:`)
+                    for (const item in list) {
+                        this.addOutput(list[item])
+                    }
+                } else {
+                    this.addOutput(`Your're not carrying anything.`)
+                }
+            } else if (noun === 'around') {
+                this.addOutput(game.rooms[this.state.currentRoom].description)
+            } else if (noun === 'shelf' && this.getItemByID('shelves')){
+                let item = this.getItemByID('shelves')
+                let index = command[2];
+                this.addOutput(item.shelves[index].description)
+            } else {
+                let item = this.getItemByID(noun)
+                console.log(item)
+                if (item) {
+                    this.addOutput(item.description)
+                } else {
+                    this.addOutput(this.getMessage('noSuchThing'))
+                }
             }
         }
 
@@ -114,14 +198,14 @@ export default class Game extends React.Component {
 
         return (
             <div className='game'>
-                <div className="game--status">{this.state.currentRoom}</div>
-                <div className='game--output'>
+                <div className="game--status">{game.rooms[this.state.currentRoom].title}</div>
+                <div className='game--output' ref={this.outputRef}>
                     <ul>
                         {outputItems}
                     </ul>
                 </div>
                 <div className='game--controls'>
-                    <input onKeyPress={this.detectSubmit} value={this.state.input} onChange={this.handleChange} className='controls--field'></input>
+                    <input ref={this.inputRef} onKeyPress={this.detectSubmit} value={this.state.input} onChange={this.handleChange} className='controls--field'></input>
                     <button onClick={this.handleInput} className='controls--button'>Enter</button>
                 </div>
             </div>
