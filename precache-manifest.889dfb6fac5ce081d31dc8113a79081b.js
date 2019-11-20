84e855455dd6f0d36270930014a37eec961fbe86
Max Kohler <hi@maxkoehler.com>
2019-11-20 10:25:09 +0000

    Updates

---
+++ precache-manifest.889dfb6fac5ce081d31dc8113a79081b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f09195c0361ebc9e1fd24bd663758e91",
+    "url": "/awesomephant/tate-explorer/index.html"
+  },
+  {
+    "revision": "cb39c4400f616a37fc40",
+    "url": "/awesomephant/tate-explorer/static/css/main.d31a2986.chunk.css"
+  },
+  {
+    "revision": "5c4a43c12253e12cbc92",
+    "url": "/awesomephant/tate-explorer/static/js/2.88d4d626.chunk.js"
+  },
+  {
+    "revision": "cb39c4400f616a37fc40",
+    "url": "/awesomephant/tate-explorer/static/js/main.1697d1ca.chunk.js"
+  },
+  {
+    "revision": "a68d4422972171ff656f",
+    "url": "/awesomephant/tate-explorer/static/js/runtime-main.f0f3ae2d.js"
+  }
+]);
